24e4423c121567041bac0c79ddd51c7ee2c7b55a
bryanbarreto <bryan94barreto@gmail.com>
2022-02-09 00:45:58 -0500

    clean up

--- src/main.cc
+++ src/main.cc
@@ -7,35 +7,39 @@
 using namespace sf;
 using namespace std;
 
-int main(int argc, char** argv) {
-
+RenderWindow window;
+Texture enemyTexture;
+Texture playerTexture;
+Texture bulletTexture;
+Player* defender = nullptr; //No memory reserved
+Baddie* killem = nullptr;
+Bullet* pew = nullptr;
+int init() {
 	
-
-	RenderWindow window(VideoMode(128, 128), "Totally Invading Space");
-	window.setKeyRepeatEnabled(false);
-	window.setFramerateLimit(60);
-	
-	Texture enemyTexture;
-	enemyTexture.setSmooth(false);
-	enemyTexture.setRepeated(false);
 	if (!enemyTexture.loadFromFile("res/resource/enemy.png")) {
 		return EXIT_FAILURE;
 	}
 
-	Texture playerTexture;
 	if (!playerTexture.loadFromFile("res/resource/player.png")) {
 		return EXIT_FAILURE;
 	}
 
-	Texture bulletTexture;
 	if (!bulletTexture.loadFromFile("res/resource/bullet.png")) {
 		return EXIT_FAILURE;
 	}
 
-	Player defender (playerTexture);
-	Baddie killem (enemyTexture);
-	Bullet* pew = nullptr;
+	defender = new Player(playerTexture); //reserves memory for new object
+	killem = new Baddie(enemyTexture);
 
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv) {
+	if (init() != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	window.create(VideoMode(128, 128), "Totally Invading Space");
+	window.setKeyRepeatEnabled(false);
+	window.setFramerateLimit(60);
 
 	while(window.isOpen()) {
 		Event currentEvent;
@@ -51,17 +55,17 @@ int main(int argc, char** argv) {
 							break;
 						case Keyboard::Key::A:
 						case Keyboard::Key::Left: //Move Left
-							defender.playerIsMovingLeft = true;
+							defender->playerIsMovingLeft = true;
 							break;
 						case Keyboard::Key::D:
 						case Keyboard::Key::Right: //Move Right
-							defender.playerIsMovingRight = true;
+							defender->playerIsMovingRight = true;
 							break;
 						case Keyboard::W:
 						case Keyboard::Up:
 						case Keyboard::Space:
 							if (!pew)
-								pew = new Bullet(bulletTexture, defender.getX() + 3);
+								pew = new Bullet(bulletTexture, defender->getX() + 3);
 							break;
 					}
 					break;
@@ -70,23 +74,23 @@ int main(int argc, char** argv) {
 					switch(currentEvent.key.code) {
 						case Keyboard::Key::A:
 						case Keyboard::Key::Left:
-							defender.playerIsMovingLeft = false;
+							defender->playerIsMovingLeft = false;
 							break;
 						case Keyboard::Key::D:
 						case Keyboard::Key::Right:
-							defender.playerIsMovingRight = false;
+							defender->playerIsMovingRight = false;
 							break;
 					}
 			}
 
 		}
 
-		defender.update();
-		killem.update();
+		defender->update();
+		killem->update();
 		if (pew) {
 			pew->update();
-			if (pew->collision().intersects(killem.collision())) {
-				delete pew;
+			if (pew->collision().intersects(killem->collision())) {
+				delete pew; //frees memory
 				pew = nullptr;
 			}
 			else if (pew->getY() <= -6) {
@@ -98,10 +102,13 @@ int main(int argc, char** argv) {
 		if (pew) {
 			pew->draw(window);
 		}
-		defender.draw(window);
-		killem.draw(window);
+		defender->draw(window);
+		killem->draw(window);
 		window.display();
 	}
-
+	delete defender;
+	delete killem;
+	if (pew)
+		delete pew;
 	return EXIT_SUCCESS;
 }
